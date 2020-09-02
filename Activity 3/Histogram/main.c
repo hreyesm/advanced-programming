ca71be71f30a2b14bf91c665934e633b769f1e80
hreyesm <hectorreyesmanrique@gmail.com>
2020-09-01 20:15:46 -0500

    Update Histogram: Generates histogram

--- Activity 3/Histogram/main.c
+++ Activity 3/Histogram/main.c
@@ -16,57 +16,58 @@
 #include <dirent.h>
 
 #define W 1024 // Window
-#define H 30 // Max stars in histogram - 1
-
-typedef struct {
-    off_t size;
-    int rangeStart;
-    int rangeEnd;
-} entry;
+#define H 30 // Max stars in histogram minus 1
 
 typedef struct {
     int nFiles;
     int rangeStart;
     int rangeEnd;
+    char *range;
     char *histogram;
 } urn;
 
-void printEntry(entry *e) {
-    printf("\n\tSize: %lld bytes", e->size);
-    printf("\nRange: %d-%d bytes\n", e->rangeStart, e->rangeEnd);
-}
-
 void printUrn(urn *u) {
-    printf("%10d%10d%10d%40s", u->rangeStart, u->rangeEnd, u->nFiles, u->histogram);
-    printf("\n");
+    printf("%10d%10d%10d%40s\n", u->rangeStart, u->rangeEnd, u->nFiles, u->histogram);
 }
 
-void traverse(urn **urns, entry **entries, char *basePath, int *count, int *max) {
+void traverse(urn **urns, char *basePath, int *count, int *max) {
     char *path = (char *) malloc(sizeof(char) * 1000);
     struct dirent *dp;
     struct stat st;
+    off_t size;
+    int rangeStart;
+    int rangeEnd;
+    char *s1;
+    char *s2;
+    urn *u;
     DIR *dir = opendir(basePath);
     if (!dir) {
-        printf("\t%s\n", basePath);
+        // printf("\t%s\n", basePath);
         stat(basePath, &st);
-        (*entries + *count)->size = st.st_size;
-        (*entries + *count)->rangeStart = ceil((*entries + *count)->size / W) * W;
-        (*entries + *count)->rangeEnd = (*entries + *count)->rangeStart + W - 1;
-        urn *u = *urns - 1;
+        size = st.st_size;
+        rangeStart = ceil(size / W) * W;
+        rangeEnd = rangeStart + W - 1;
+        u = *urns - 1;
         for (; u < *urns + *count; u++) {
             if (u->rangeEnd == 0) {
-                u->rangeStart = (*entries + *count)->rangeStart;
-                u->rangeEnd = (*entries + *count)->rangeEnd;
+                u->range = (char *) malloc(sizeof(char) * 20);
+                u->rangeStart = rangeStart;
+                u->rangeEnd = rangeEnd;
+                s1 = (char *) malloc(sizeof(char) * 10);
+                s2 = (char *) malloc(sizeof(char) * 10);
+                sprintf(s1, "%d-", u->rangeStart);
+                sprintf(s2, "%d", u->rangeEnd);
+                strcpy(u->range, strcat(s1, s2));
+                printf("%s\n", u->range);
                 u->nFiles = 1;
                 break;
-            } else if (u->rangeEnd == (*entries + *count)->rangeEnd) {
+            } else if (u->rangeEnd == rangeEnd) {
                 (u->nFiles)++;
                 *max = fmax((double) *max, (double) u->nFiles);
                 break;
             }
         }
         (*count)++;
-        free(path);
         return;
     }
     while ((dp = readdir(dir)) != NULL) {
@@ -74,7 +75,7 @@ void traverse(urn **urns, entry **entries, char *basePath, int *count, int *max)
             strcpy(path, basePath);
             strcat(path, "/");
             strcat(path, dp->d_name);
-            traverse(urns, entries, path, count, max);
+            traverse(urns, path, count, max);
         }
     }
     closedir(dir);
@@ -83,38 +84,40 @@ void traverse(urn **urns, entry **entries, char *basePath, int *count, int *max)
 
 int main(int argc, char const *argv[]) {
     // Urns
-    entry *entries = (entry *) malloc(sizeof(entry) * 1000000);
     urn *urns = (urn *) malloc(sizeof(urn) * 1000);
     char *path = (char *) malloc(sizeof(char) * 1000);
     int count = 0;
     int max = 1;
     printf("\nHistogram\n---------\n\nPath: ");
     scanf("%s", path);
-    traverse(&urns, &entries, path, &count, &max);
+    traverse(&urns, path, &count, &max);
 
     // Histograms
     urn *u = urns - 1;
     int nStars = 0;
+    char *c;
     for (; u->rangeEnd != 0; u++) {
         nStars = u->nFiles * H / max;
         u->histogram = (char *) malloc(sizeof(char) * nStars);
         strcpy(u->histogram, "*");
-        char *c = u->histogram + 1;
+        c = u->histogram + 1;
         for (; c < u->histogram + nStars; c++) {
             strcpy(c, "*");
         }
     }
     
     // Print Urns
-    printf("\n%10s%10s%10s%40s\n", "", "Range", "Files", "Histogram");
-    printf("%10s%10s%10s%40s\n", "", "-----", "-----", "---------");
+    printf("\n%10s%10s%10s%40s\n", "", "Urn (Size Range)", "File", "Histogram");
+    printf("%10s%10s%10s%40s\n", "", "---", "----", "---------");
     u = urns - 1;
+    int i = 0;
     for (; u->rangeEnd != 0; u++) {
         printUrn(u);
+        i++;
     }
-    printf("\nTotal: %d\n", count);
+    printf("%10s%10s%10s%40s\n", "-----", "---", "----", "");
+    printf("%10s%10d%10d%40s\n", "Total", i, count, "");
 
-    free(entries);
     free(urns);
     free(path);
     return 0;
