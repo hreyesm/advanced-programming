70d84fecc58992f591bd497c6eaf37ecb86c7c9c
hreyesm <hectorreyesmanrique@gmail.com>
2020-10-13 19:10:55 -0500

    Update main.c

--- Activity 8/main.c
+++ Activity 8/main.c
@@ -1,18 +1,44 @@
+
+/*
+  Solución al problema del productor-consumidor utilizando variables de condición
+  - Buena solución porque no tiene espera activa
+  - Además, brinda sincronización (a quien le corresponde el turno)
+  - Funciones pthread_cond_wait() y pthread_cond_broadcast()
+  - NP productores y NC consumidores
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
 #include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define ATMS 8
+#define BUFF_SIZE 10
+
 #define G_USERS 10
-#define E_USERS 5
+#define B_USERS 5
+#define ATMS 8
 
-int lowerAtm = 3, upperAtm = 5;
 int lowerGUser = 1, upperGUser = 2;
 int lowerEUser = 3, upperEUser = 4;
+int lowerAtm = 5, upperAtm = 6;
+
+int gQueue[BUFF_SIZE];
+int bQueue[BUFF_SIZE];
+
+int in = 0;
+int out = 0;
+
+int total = 0;
+
+int usersCreated = 0, usersDispatched = 0;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t consume_t = PTHREAD_COND_INITIALIZER;
+pthread_cond_t produce_t = PTHREAD_COND_INITIALIZER;
+
+void * agent(void *);
+void * atms(void *);
 
 double operation(int lower, int upper) {
     time_t start = time(NULL);
@@ -21,74 +47,104 @@ double operation(int lower, int upper) {
     return (double)(end - start);
 }
 
-void *generalOps (void * arg) {
-    int tid = (int) arg;
-    int i;
-    double totalTime = 0.0;
+int main(int argc, const char * argv[]) {  
+    srand((int) time(NULL));
+    
+    int nThreads = ATMS + 1;
+    
+    pthread_t *bank = (pthread_t *) malloc (sizeof(pthread_t) * nThreads);
+    
+    int index = 0;
+    
+    pthread_t *aux = bank;
+    
+    /* Crear los productores */
+    pthread_create(aux, NULL, agent, (void *) index);
+    printf("Agent was created\n");
 
-    pthread_mutex_lock(&mutex);
-    totalTime = operation(lowerAtm, upperAtm);
-    printf("GO - User %d, Time spent: %.2f\n", tid, totalTime);
-    pthread_mutex_unlock(&mutex);
+    ++aux;
 
-    pthread_exit(0);
-}
-
-void *businessOps (void * arg) {
-    int tid = (int) arg;
-    int i;
-    double totalTime = 0.0;
-
-    pthread_mutex_lock(&mutex);
-    totalTime = operation(lowerAtm, upperAtm);
-    printf("BO - User %d, Time spent: %.2f\n", tid, totalTime);
-    pthread_mutex_unlock(&mutex);
-    
-    pthread_exit(0);
-}
-
-int main(int argc, const char * argv[]) {
-    pthread_t *gid, *eid;
-    int nATMs, gUsers, eUsers, i;
-    double totalTime;
-    
-    srand((unsigned int) time(NULL));
-    
-    nATMs = ATMS;
-    gUsers = G_USERS;
-    eUsers = E_USERS;
-    
-    gid = malloc(gUsers * sizeof(pthread_t));
-    eid = malloc(eUsers * sizeof(pthread_t));
-    
-    printf("The bank is open...\n");
-    
-    for (i = 0; i < gUsers; i++) {
-        totalTime = operation(lowerGUser, upperGUser);
-        printf("User %d, Arrival time: %.2f\n", i, totalTime);
-        pthread_create(gid + i, NULL, generalOps, (void *) i);
+    /* Crear los consumidores */    
+    for (; aux < bank + nThreads; ++aux) {
+        printf("ATM %d was created\n", ++index);
+        pthread_create(aux, NULL, atms, (void *) index);
     }
 
-    for (i = 0; i < eUsers; i++) {
-        totalTime = operation(lowerEUser, upperEUser);
-        printf("User %d, Arrival time: %.2f\n", i + gUsers, totalTime);
-        pthread_create(eid + i, NULL, businessOps, (void *) i + gUsers);
+    /* Adjuntar los hilos */
+    for (aux = bank; aux < (bank + nThreads); ++aux) {
+        pthread_join(*aux, NULL);
     }
-
-    printf("%d clients were attended...\n", gUsers + eUsers);
-
-    for (i = 0; i < gUsers; i++) {
-        pthread_join(*(gid + i), NULL);
-    }
-
-    for (i = 0; i < eUsers; i++) {
-        pthread_join(*(eid + i), NULL);
-    }    
     
-    printf("The bank is closed..\n");
-    
-    free(gid);
-    free(eid);
+    free(bank);
     
     return 0;
+
+}
+
+void *agent(void *arg) {
+    int id = (int) arg;  
+    printf("Agent starts\n");
+    
+    while (usersCreated < (G_USERS + B_USERS - 1)) {  
+        usleep(rand() % 500);
+        pthread_mutex_lock(&mutex);
+        
+        if (total < BUFF_SIZE) {     
+            /* Client with a general operation */
+            gQueue[in] = usersCreated;
+            printf("+++ (Agent %d) Client %d has arrived to the queue\n", id, gQueue[in]);
+            ++usersCreated;
+            ++in;
+            in %= BUFF_SIZE;
+            ++total;
+            if (total == 1) {
+                pthread_cond_broadcast(&consume_t);
+            }
+        } else {
+            /* El buffer está lleno, se va a dormir */        
+            printf("-------------- Agent %d is asleep ------------\n", id);
+            pthread_cond_wait(&produce_t, &mutex);
+            printf("-------------- Agent %d is awake ------------\n", id);
+        }
+        
+        pthread_mutex_unlock(&mutex);
+        
+    }
+    
+    pthread_exit(NULL);
+}
+
+void * atms(void * arg) {
+    int id = (int) arg;
+    printf("ATM %d has started working\n", id);
+
+    while (usersDispatched < (G_USERS + B_USERS)) {    
+        /* Asumir que trabajan a diferentes velocidades */
+        usleep(rand() % 500);
+        pthread_mutex_lock(&mutex);
+        
+        if (total > 0) { // Hay un elemento en el buffer
+            /* Consume un elemento */
+            printf("--- ATM %d dispatched client %d\n", id, gQueue[out]);
+            ++usersDispatched;
+            ++out;
+            out %= BUFF_SIZE;
+            --total;
+            
+            if (total == (BUFF_SIZE - 1) ) {
+                pthread_cond_broadcast(&produce_t);
+            }
+            
+        } else {
+            /* El buffer está vacío, se va a dormir */
+            printf("-------------- ATM %d is asleep ------------\n", id);
+            pthread_cond_wait(&consume_t, &mutex);
+            printf("-------------- ATM %d is awake ------------\n", id);
+        }
+        
+        pthread_mutex_unlock(&mutex);
+        
+    }
+    
+    pthread_exit(NULL);
 }
