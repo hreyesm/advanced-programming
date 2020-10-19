a0217cb499e3f4f6b66eecffa4f45e93ed68f9b2
hreyesm <hectorreyesmanrique@gmail.com>
2020-10-19 18:07:11 -0500

    Create branch four-threads

--- Activity 9/main.c
+++ Activity 9/main.c
@@ -15,83 +15,107 @@ int men = 0;
 int peopleInBathroom = 0;
 int womenInQueue = 0, menInQueue = 0;
 
-void *womenInBathroom(void *);
-void *menInBathroom(void *);
+void *womanEnters(void *);
+void *manEnters(void *);
+void *womanLeaves(void *);
+void *manLeaves(void *);
 
 int main(int argc, const char *argv[]) {
     srand((int) time(NULL));  
-    pthread_t *people = (pthread_t *) malloc (sizeof(pthread_t) * PEOPLE);
+    pthread_t *people = (pthread_t *) malloc (sizeof(pthread_t) * PEOPLE * 2);
     int peopleCreated = 0;
     int gender = 0;
+    int womenIndex = 0;
+    int menIndex = 0;
     if (peopleInBathroom == 0) {
         printf("Bathroom is empty\n");
     }
-    while (peopleCreated < PEOPLE) {
+    while (peopleCreated < PEOPLE * 2) {
         gender = rand() % 2;
         if (gender == 0) {
-            pthread_create(people + peopleCreated, NULL, womenInBathroom, NULL);
+            pthread_create(people + peopleCreated, NULL, womanEnters, (void *) ++womenIndex);
+            ++peopleCreated;
+            pthread_create(people + peopleCreated, NULL, womanLeaves, (void *) womenIndex);
         } else {
-            pthread_create(people + peopleCreated, NULL, menInBathroom, NULL);
+            pthread_create(people + peopleCreated, NULL, manEnters, (void *) ++menIndex);
+            ++peopleCreated;
+            pthread_create(people + peopleCreated, NULL, manLeaves, (void *) menIndex);
         }
         ++peopleCreated;
     }
-    for (pthread_t *aux = people; aux < people + PEOPLE; ++aux) {
+    for (pthread_t *aux = people; aux < people + PEOPLE * 2; ++aux) {
         pthread_join(*aux, NULL);
     }
     free(people);
     return 0;
 }
 
-void *womenInBathroom(void *id) {
+void *womanEnters(void *id) {
+    int index = (intptr_t) id;
     pthread_mutex_lock(&mutex);
-    printf("(W) WOMAN has arrived to the queue (%d waiting)\n", ++womenInQueue);
+    printf("(W) WOMAN %d has arrived to the queue (%d waiting)\n", index, ++womenInQueue);
     while (womenInQueue > 0) {
         if (peopleInBathroom == 0 || women > 0) {
-            printf("(W) --> WOMAN has entered the bathroom (%d waiting)\n", --womenInQueue);
+            printf("(W) --> WOMAN %d has entered the bathroom (%d waiting)\n", index, --womenInQueue);
             ++women;
             ++peopleInBathroom;
             pthread_mutex_unlock(&mutex);
             usleep(300);
-            pthread_mutex_lock(&mutex);
-            printf("(W) WOMAN has left the bathroom -->\n");
-            --women;
-            --peopleInBathroom;
-            if (peopleInBathroom == 0) {
-                printf("Bathroom is empty\n");
-                pthread_cond_broadcast(&mutex_m);
-            }        
         } else {
-            printf("(W) WOMAN cannot enter the bathroom\n");
+            printf("(W) WOMAN % d cannot enter the bathroom\n", index);
             pthread_cond_wait(&mutex_w, &mutex);
-            printf("(W) WOMAN can enter the bathroom\n");
+            printf("(W) WOMAN %d can enter the bathroom\n", index);
         }
-        pthread_mutex_unlock(&mutex);
     }
     pthread_exit(NULL);
 }
 
-void *menInBathroom(void *id) {
+void *manEnters(void *id) {
+    int index = (intptr_t) id;
     pthread_mutex_lock(&mutex);
-    printf("(M) MAN has arrived to the queue (%d waiting)\n", ++menInQueue);
-    while (menInQueue > 0) {    
+    printf("(M) MAN %d has arrived to the queue (%d waiting)\n", index, ++menInQueue);
+    while (menInQueue > 0) {
         if (peopleInBathroom == 0 || men > 0) {
-            printf("(M) --> MAN has entered the bathroom (%d waiting)\n", --menInQueue);
+            printf("(M) --> MAN %d has entered the bathroom (%d waiting)\n", index, --menInQueue);
             ++men;
             ++peopleInBathroom;
             pthread_mutex_unlock(&mutex);
-            usleep(300);        
-            pthread_mutex_lock(&mutex);
-            printf("(M) MAN has left the bathroom -->\n");
-            --men;
-            --peopleInBathroom;
-            if (peopleInBathroom == 0) {
-                printf("Bathroom is empty\n");
-                pthread_cond_broadcast(&mutex_w);
-            }
-        } else {        
-            printf("(M) MAN cannot enter the bathroom\n");
+            usleep(300);
+        } else {
+            printf("(M) MAN %d cannot enter the bathroom\n", index);
             pthread_cond_wait(&mutex_m, &mutex);
-            printf("(M) MAN can enter the bathroom\n");
+            printf("(M) MAN %d can enter the bathroom\n", index);
+        }
+    }
+    pthread_exit(NULL);
+}
+
+void *womanLeaves(void *id) {
+    int index = (intptr_t) id;
+    pthread_mutex_lock(&mutex);
+    while (women > 0) {
+        printf("(W) WOMAN %d has left the bathroom -->\n", index);
+        --women;
+        --peopleInBathroom;
+        if (peopleInBathroom == 0) {
+            printf("Bathroom is empty\n");
+            pthread_cond_broadcast(&mutex_m);
+        }
+    }
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
+}
+
+void *manLeaves(void *id) {
+    int index = (intptr_t) id;
+    pthread_mutex_lock(&mutex);
+    while (men > 0) {
+        printf("(M) MAN %d has left the bathroom -->\n", index);
+        --men;
+        --peopleInBathroom;
+        if (peopleInBathroom == 0) {
+            printf("Bathroom is empty\n");
+            pthread_cond_broadcast(&mutex_w);
         }
     }
     pthread_mutex_unlock(&mutex);
