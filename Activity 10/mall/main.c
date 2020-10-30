8421e47f199357aeea3a4704e16b46acee72191e
Dani Vignau <dvigleo@gmail.com>
2020-10-29 20:19:51 -0600

    Attempt to make robots buy products and add the weight to the section

--- Activity 10/mall/main.c
+++ Activity 10/mall/main.c
@@ -15,8 +15,10 @@
 
 #define SECTIONS 3
 #define SECTION_WEIGHT 2
-#define ROBOTS 3
+#define ROBOTS 2
 #define ROBOT_WEIGHT 1
+#define NO 0
+#define YES 1
 
 int mall[SECTIONS];
 
@@ -44,16 +46,32 @@ int main(int argc, const char *argv[]) {
 void *robot(void *arg) {
     int id = (intptr_t) arg;
     int i = 0;
+    int robotHasToBuy = 0;
+    int rangeProductWeight = 0;
+    int productWeight = 0;
+    int robotWeight = ROBOT_WEIGHT; // necessary in case the robot doesnt have to buy in the section
     usleep(rand() % 300);
     while (i < SECTIONS) {
         pthread_mutex_lock(&mutex_in);
         if (mall[i] + ROBOT_WEIGHT <= SECTION_WEIGHT) {
             mall[i] += ROBOT_WEIGHT;
             printf("--> ROBOT %d has entered SECTION %d (accumulated weight = %d)\n", id, i, mall[i]);
+            robotHasToBuy = rand() % 2;
+            if (robotHasToBuy == YES) {
+                rangeProductWeight = SECTION_WEIGHT - mall[i]; // attempt to find the range of avaiable numbers where the product weight is and still be within the max section weight
+                productWeight = rand() % SECTION_WEIGHT;
+                printf("Product Weight = %d\n", productWeight);
+                robotWeight += productWeight;
+                mall[i] += robotWeight - ROBOT_WEIGHT; // we don't want to add twice the robot weight
+                printf("--> ROBOT %d has bought a product of weight %d (accumulated weight = %d)\n", id, productWeight, mall[i]);
+            } else {
+                printf("ROBOT %d does not have tu purchase anything in SECTION %d\n", id, i);
+            }
             pthread_mutex_unlock(&mutex_in);
             usleep(rand() % 300);
             pthread_mutex_lock(&mutex_out);
-            mall[i] -= ROBOT_WEIGHT;
+            mall[i] -= robotWeight; // reset the section weight;
+            robotWeight = ROBOT_WEIGHT; // reset the robot weight;
             printf("<-- ROBOT %d has left SECTION %d (accumulated weight = %d)\n", id, i, mall[i]);
             pthread_mutex_unlock(&mutex_out);
             i++;
